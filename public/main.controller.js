7e3196765a60f37275366cf418a5f35a99bfd245
jeanmariano <jeandmariano@gmail.com>
2015-04-19 23:43:29 -0400

    completed server pulls, categories, and keeping score

--- public/main.controller.js
+++ public/main.controller.js
@@ -1,10 +1,35 @@
-app.controller('MainController', function ($scope, whateverName) {
-	$scope.flashCards = whateverName;
+app.controller('MainController', function ($scope, FlashCardsFactory, ScoreFactory) {
+    $scope.activeCat;
+
+    FlashCardsFactory.getFlashCards().then(
+        function(response) {
+            $scope.flashCards = response;
+        });
 
 	$scope.answerQuestion = function (answer, flashCard) {
 		if (!flashCard.answered) {
 			flashCard.answered = true;
 			flashCard.answeredCorrectly = answer.correct;
+            var score = ScoreFactory;
+            if (answer.correct)
+                score.correct++;
+            else
+                score.incorrect++;
 		}
 	}
+
+    $scope.getCategoryCards = function (category) {
+        FlashCardsFactory.getFlashCards(category).then(
+        function(response) {
+            $scope.flashCards = response;
+        });
+        $scope.activeCat = category;
+    }
+
+    $scope.categories = [
+        'MongoDB',
+        'Express',
+        'Angular',
+        'Node'
+    ];
 });
